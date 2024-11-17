2d1020f0c7f67142169d959ffd2f71eb98902968
kalai64 <kalaikabil6464@gmail.com>
2024-11-17 14:42:42 +0530

    Link-Update

--- frontend/src/components/ItemsView.jsx
+++ frontend/src/components/ItemsView.jsx
@@ -41,7 +41,7 @@ function ItemsView({ searchQuery }) {
 
     <div className="d-flex justify-content-center align-items-center" style={{margin: "50px"}}>
       <div className="w-50 rounded p-3">
-      <table class="table">
+      <table className="table">
           <thead>
             <tr>
               <th scope="col">No</th>
@@ -84,47 +84,6 @@ function ItemsView({ searchQuery }) {
 
     </div>
     
-
-
-
-
-      {/* <section className="py-5">
-        <div className="container px-4 px-lg-5 mt-5">
-          <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            {filteredItems.map((e, i) => {
-              return (
-                <div className="col mb-5" key={i}>
-                  <div className="card h-100">
-                    <img
-                      className="card-img-top"
-                      src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-                      alt="..."
-                    />
-                    <div className="card-body p-4">
-                      <div className="text-center">
-                        <h5 className="fw-bolder">{e.product_name}</h5>$
-                        {e.price}
-                      </div>
-                    </div>
-                    <div className=" card-footer p-4 pt-0 border-top-0 bg-transparent">
-                      <Link to={`/item/${e._id}`} className="btn btn-primary">
-                        View
-                      </Link>
-                      <button
-                        type="button"
-                        className="btn btn-danger"
-                        onClick={() => handleDelete(e._id)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </section> */}
     </>
   );
 }
